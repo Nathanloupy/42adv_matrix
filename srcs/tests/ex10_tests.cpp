763ab206ed9edfb315fe30719b857adea30c8a74
Nathanloupy <94622983+Nathanloupy@users.noreply.github.com>
2025-03-07 17:25:50 +0100

    feat: better tests for ex10 and ex11

--- srcs/tests/ex10_tests.cpp
+++ srcs/tests/ex10_tests.cpp
@@ -5,6 +5,14 @@ void test_matrix_ex10(void)
 	static int i = 0;
 	{
 		std::cout << "Test Matrix ex10 " << ++i << ":" << std::endl;
+		Matrix<float> u(1, 1, {9.0f});
+		std::cout << "u: " << std::endl;
+		std::cout << u;
+		std::cout << "row_echelon of u: " << std::endl;
+		std::cout << u.row_echelon();
+	}
+	{
+		std::cout << "Test Matrix ex10 " << ++i << ":" << std::endl;
 		Matrix<float> u(3, 3, {1.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 1.0f});
 		std::cout << "u: " << std::endl;
 		std::cout << u;
--- srcs/tests/ex11_tests.cpp
+++ srcs/tests/ex11_tests.cpp
@@ -1,37 +1,45 @@
 #include "commons.hpp"
 
-void test_matrix_determinant_2x2(void)
+void test_matrix_ex11(void)
 {
-	Matrix<float> A2(2, 2, {1.0f, -1.0f, -1.0f, 1.0f});
-	std::cout << "A2: " << std::endl;
-	std::cout << A2;
-	std::cout << "determinant of A2: " << std::endl;
-	std::cout << A2.determinant() << std::endl;
-}
-
-void test_matrix_determinant_3x3(void)
-{
-	Matrix<float> A3(3, 3, {8.0f, 4.0f, 7.0f, 5.0f, 7.0f, 6.0f, -2.0f, 20.0f, 1.0f});
-	std::cout << "A3: " << std::endl;
-	std::cout << A3;
-	std::cout << "determinant of A3: " << std::endl;
-	std::cout << A3.determinant() << std::endl;
-}
-
-void test_matrix_determinant_4x4(void)
-{
-	Matrix<float> A4(4, 4, {8.0f, 4.0f, 8.0f, 28.0f, 5.0f, 2.5f, 5.0f, -4.0f, -2.0f, 20.0f, 1.0f, 17.0f, 4.0f, 4.0f, 4.0f, 1.0f});
-	std::cout << "A4: " << std::endl;
-	std::cout << A4;
-	std::cout << "determinant of A4: " << std::endl;
-	std::cout << A4.determinant() << std::endl;
+	size_t i = 0;
+	{
+		std::cout << "Test Matrix ex11 " << ++i << ":" << std::endl;
+		Matrix<float> A1(1, 1, {9.0f});
+		std::cout << "A1: " << std::endl;
+		std::cout << A1;
+		std::cout << "determinant of A1: " << std::endl;
+		std::cout << A1.determinant() << std::endl;
+	}
+	{
+		std::cout << "Test Matrix ex11 " << ++i << ":" << std::endl;
+		Matrix<float> A2(2, 2, {1.0f, -1.0f, -1.0f, 1.0f});
+		std::cout << "A2: " << std::endl;
+		std::cout << A2;
+		std::cout << "determinant of A2: " << std::endl;
+		std::cout << A2.determinant() << std::endl;
+	}
+	{
+		std::cout << "Test Matrix ex11 " << ++i << ":" << std::endl;
+		Matrix<float> A3(3, 3, {8.0f, 4.0f, 7.0f, 5.0f, 7.0f, 6.0f, -2.0f, 20.0f, 1.0f});
+		std::cout << "A3: " << std::endl;
+		std::cout << A3;
+		std::cout << "determinant of A3: " << std::endl;
+		std::cout << A3.determinant() << std::endl;
+	}
+	{
+		std::cout << "Test Matrix ex11 " << ++i << ":" << std::endl;
+		Matrix<float> A4(4, 4, {8.0f, 4.0f, 8.0f, 28.0f, 5.0f, 2.5f, 5.0f, -4.0f, -2.0f, 20.0f, 1.0f, 17.0f, 4.0f, 4.0f, 4.0f, 1.0f});
+		std::cout << "A4: " << std::endl;
+		std::cout << A4;
+		std::cout << "determinant of A4: " << std::endl;
+		std::cout << A4.determinant() << std::endl;
+	}
 }
 
 void test_ex11(void)
 {
 	std::cout << "Ex11 tests : [Matrix] Determinant" << std::endl;
-	test_matrix_determinant_2x2();
-	test_matrix_determinant_3x3();
-	test_matrix_determinant_4x4();
+	test_matrix_ex11();
 	std::cout << "--------------------------------" << std::endl;
 }
